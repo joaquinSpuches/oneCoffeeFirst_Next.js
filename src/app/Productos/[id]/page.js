a94c1e433b67723c20a6f7f1bd90fa51cc6855c2
joaquinSpuches <spuchesjoaquin@gmail.com>
2024-02-07 23:35:06 +0100

    cambios en el formateo

--- src/app/Productos/[id]/page.js
+++ src/app/Productos/[id]/page.js
@@ -4,51 +4,43 @@ import Text from "../../Components/Text";
 import Image from "next/image";
 
 import Title from "../../Components/Title";
-const Productos =async (id)=> {
-    
-    const productid = id.params.id - 1
-    
-    const data = await fetch('http://Localhost:3000/api/productos', {
-        cache:'force-cache',
-        next:{
-            tags:['productos']
-        }}).then(r =>r.json())
+const Productos = async (id) => {
+  const productid = id.params.id - 1;
 
-    const producto = data[productid]
+  const data = await fetch("http://Localhost:3000/api/productos", {
+    cache: "force-cache",
+    next: {
+      tags: ["productos"],
+    },
+  }).then((r) => r.json());
 
-        return (
-        
-            <div key={id} className=" p-5 bg-white flex flex-col items-center "> 
-            
-                <div className="pb-10  ">
-                
-                    <Precio text={`USD ${producto.precio}`} />  
-                    <Title text={`${producto.titulo}`}/>
-                
-                    <div className=" mb-7 rounded-md bg-gray-100 flex justify-center px-10 ">
-                
-                        <Image alt="DetalleProducto" src={'https://raw.githubusercontent.com/joaquinSpuches/oneCoffeeFirst/main/Front/public/Asset%204.png'}  
-                            className="p-12"
-                            width={400}
-                            height={400} 
-                        />    
-                
-                    </div>
-            
-                </div>  
-                    <div className=" max-w-2xl" >
-                
-                    <MiniTitle text={`${producto.titulo}`}  />
-                    <Text text='Etiopía es considerada la cuna del café, ya que fue allí donde se descubrió esta planta hace más de mil años. El café etíope se caracteriza por su gran diversidad de sabores, aromas y perfiles, debido a las diferentes regiones, altitudes, variedades y procesos que se emplean en su cultivo y elaboración. Algunas de las zonas más famosas por su café son Yirgacheffe, Sidamo, Harrar y Limu. '/>
-                    <Text text=' El café etíope suele tener un cuerpo ligero, una acidez cítrica y floral, y notas frutales, especiadas y dulces. Es un café ideal para disfrutar solo o con leche, y se puede preparar con diferentes métodos, como el filtro, el espresso o el tradicional jebena. El café etíope es uno de los más apreciados por los amantes del café de especialidad, ya que ofrece una experiencia única y exótica.'/>
-                
-                </div>
-    
-            </div>
-        
-        )
-    }
+  const producto = data[productid];
 
+  return (
+    <div key={id} className=" p-5 bg-white flex flex-col items-center ">
+      <div className="pb-10  ">
+        <Precio text={`USD ${producto.precio}`} />
+        <Title text={`${producto.titulo}`} />
 
+        <div className=" mb-7 rounded-md bg-gray-100 flex justify-center px-10 ">
+          <Image
+            alt="DetalleProducto"
+            src={
+              "https://raw.githubusercontent.com/joaquinSpuches/oneCoffeeFirst/main/Front/public/Asset%204.png"
+            }
+            className="p-12"
+            width={400}
+            height={400}
+          />
+        </div>
+      </div>
+      <div className=" max-w-2xl">
+        <MiniTitle text={`${producto.titulo}`} />
+        <Text text="Etiopía es considerada la cuna del café, ya que fue allí donde se descubrió esta planta hace más de mil años. El café etíope se caracteriza por su gran diversidad de sabores, aromas y perfiles, debido a las diferentes regiones, altitudes, variedades y procesos que se emplean en su cultivo y elaboración. Algunas de las zonas más famosas por su café son Yirgacheffe, Sidamo, Harrar y Limu. " />
+        <Text text=" El café etíope suele tener un cuerpo ligero, una acidez cítrica y floral, y notas frutales, especiadas y dulces. Es un café ideal para disfrutar solo o con leche, y se puede preparar con diferentes métodos, como el filtro, el espresso o el tradicional jebena. El café etíope es uno de los más apreciados por los amantes del café de especialidad, ya que ofrece una experiencia única y exótica." />
+      </div>
+    </div>
+  );
+};
 
-export default Productos
+export default Productos;
--- src/app/api/productos/route.js
+++ src/app/api/productos/route.js
@@ -1,10 +1,14 @@
 import { NextResponse } from "next/server";
-import mockData from '../../../../data/productos'
+import mockData from "../../../../data/productos";
+import { revalidateTag, revalidatePath } from "next/cache";
 
-const sleep = (timer)=>{
-    return new Promise ((resolve)=>setTimeout(resolve, timer))
-}
-export async function GET(){
-    await sleep(1000)
-    return NextResponse.json(mockData)
+const sleep = (timer) => {
+  return new Promise((resolve) => setTimeout(resolve, timer));
+};
+export async function GET() {
+  await sleep(1000);
+
+  revalidateTag("productos");
+
+  return NextResponse.json(mockData);
 }

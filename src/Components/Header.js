bb35c18ca8d3fe331f4c5ceadb16c8fcf08a6f6d
joaquinSpuches <spuchesjoaquin@gmail.com>
2024-03-06 19:15:11 +0100

    cambiando imagenes de logo y avatar por las almacenadas en firebase

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,10 +9,10 @@ export default function Header(params) {
       
        <Link href='/' ><Image 
        alt='logo'
-       src={'@/public/static/logo.svg'} width={100} height={34} priority
+       src={'https://firebasestorage.googleapis.com/v0/b/onecoffeefirst-5da09.appspot.com/o/Logo.svg?alt=media&token=c1833c26-9a1a-4566-abea-6db4cbf75235'} width={100} height={34} priority
        /></Link>
         <Link href='/Perfil' className="flex flex-col items-center"> <Image  alt="perfil"
-        src={'@/public/static/avatar.png'} width={25} height={25} className="p-1" /><p className=" text-center text-xs">Perfil</p>
+        src={'https://firebasestorage.googleapis.com/v0/b/onecoffeefirst-5da09.appspot.com/o/Avatar.png?alt=media&token=02cc3d42-f659-4935-8e52-45c28930cc6b'} width={25} height={25} className="p-1" /><p className=" text-center text-xs">Perfil</p>
      </Link>
         </header>
     )

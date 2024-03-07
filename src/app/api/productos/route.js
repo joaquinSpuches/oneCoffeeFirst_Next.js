afa2a06d551a243e5bbb0793baa51ffcd3d031b1
joaquinSpuches <spuchesjoaquin@gmail.com>
2024-03-07 21:13:15 +0100

    f

--- src/app/api/productos/route.js
+++ src/app/api/productos/route.js
@@ -1,17 +1,14 @@
 import { NextResponse } from "next/server";
 import { collection, getDocs, query, where } from "firebase/firestore"; 
-import { db } from '@/firebase/config';
+import { db } from '../../../../firebase/config';
 
-export async function GET() {
+export async function GET(request, { params }) {
   
-  
-  const productosRef = collection(db, 'productos');
-  
-  
-  const q = productosRef
-  const querySnapshot = await getDocs(q);
+ 
+  const productosRef = collection(db, "productos");
+  const DocsSnapshot = await getDocs(productosRef);
 
-  const docs = querySnapshot.docs.map((doc) => doc.data());
+  const docs = DocsSnapshot.docs.map((doc) => doc.data());
 
   
   return NextResponse.json(docs);
--- src/app/api/ventas/route.js
+++ src/app/api/ventas/route.js
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
 import { collection, getDocs } from "firebase/firestore"; 
-import { db } from '@/firebase/config';
+import { db } from '../../../../firebase/config';
 
-export async function GET() {
+export async function GET(request, { params }) {
 
 
   const productosRef = collection(db, "ventas");

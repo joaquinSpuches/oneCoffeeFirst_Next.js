6adf667e1f673cfb7003012a6391289506c6da4f
joaquinSpuches <spuchesjoaquin@gmail.com>
2024-03-07 21:24:47 +0100

    ff

--- src/app/api/productos/[id]/route.js
+++ src/app/api/productos/[id]/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { doc, getDoc } from "firebase/firestore"; 
-import { db } from '../../../../firebase/config';
+import { db } from '@/firebase/config';
 
 export async function GET(request, {params} ) {
     const {id} = params
--- src/app/api/ventas/route.js
+++ src/app/api/ventas/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { collection, getDocs } from "firebase/firestore";
-import { db } from "../../../../firebase/config";
+import { db } from "@/firebase/config";
 
 export async function GET(request, { params }) {
   const productosRef = collection(db, "ventas");

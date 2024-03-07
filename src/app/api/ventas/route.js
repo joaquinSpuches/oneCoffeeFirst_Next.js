0d53b9f2a8cc6062be7435b983c8558c3d7b8026
joaquinSpuches <spuchesjoaquin@gmail.com>
2024-03-07 20:35:49 +0100

    fixing buys api

--- src/app/api/ventas/route.js
+++ src/app/api/ventas/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { collection, getDocs, query, where } from "firebase/firestore"; 
-import { db } from '../../../firebase/config';
+import { db } from '@/firebase/config';
 
 export async function GET() {
   const productosRef = collection(db, 'ventas');

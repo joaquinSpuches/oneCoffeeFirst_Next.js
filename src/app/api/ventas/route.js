a88e2ac89e0f496c2a205821d5ded2fe5264f876
joaquinSpuches <spuchesjoaquin@gmail.com>
2024-03-07 23:30:59 +0100

    trying dynamic forcedynamic

--- src/app/api/ventas/route.js
+++ src/app/api/ventas/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "@/firebase/config";
-
+export const dynamic = 'force-dynamic'
 export async function GET(request, { params }) {
 
 

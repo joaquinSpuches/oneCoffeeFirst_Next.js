26357bae1ed80ce55a56076cf058c316f8345963
joaquinSpuches <spuchesjoaquin@gmail.com>
2024-03-07 20:53:14 +0100

    f

--- src/app/api/productos/route.js
+++ src/app/api/productos/route.js
@@ -3,11 +3,15 @@ import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from '@/firebase/config';
 
 export async function GET() {
+  
+  
   const productosRef = collection(db, 'productos');
+  
+  
   const q = productosRef
   const querySnapshot = await getDocs(q);
 
-  const docs = querySnapshot.docs.map(doc => doc.data());
+  const docs = querySnapshot.docs.map((doc) => doc.data());
 
   
   return NextResponse.json(docs);
--- src/app/api/ventas/route.js
+++ src/app/api/ventas/route.js
@@ -1,15 +1,21 @@
 import { NextResponse } from "next/server";
-import { collection, getDocs, query, where } from "firebase/firestore"; 
+import { collection, getDocs } from "firebase/firestore"; 
 import { db } from '@/firebase/config';
 
 export async function GET() {
-  const productosRef = collection(db, 'ventas');
+
+
+  const productosRef = collection(db, "ventas");
+  
   const q = productosRef
+  
+  
+  
   const querySnapshot = await getDocs(q);
 
-  const docs = querySnapshot.docs.map(doc => doc.data());
+  const docs = querySnapshot.docs.map((doc) => doc.data());
 
-  
   return NextResponse.json(docs);
+  
 
 }

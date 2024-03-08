3f114abf27e641a77293336eb93e8bd91c9269b3
joaquinSpuches <spuchesjoaquin@gmail.com>
2024-03-08 03:58:40 +0100

    agregado el total en el carrito y un directorio con las compras realizadas por el usario, le falta css

--- src/Components/CartelAceptar.js
+++ src/Components/CartelAceptar.js
@@ -39,9 +39,9 @@ export default function CartelAceptar({response}) {
   
     if (param) {
     return(
-        <div className='fixed bg-black bg-opacity-10   z-50 h-screen top-0 w-full '>
+        <div className='fixed bg-black bg-opacity-50   z-50 h-screen top-0 w-full '>
            <div className="fixed top-1/2 ">
-           <h1 className='text-xl text-center  p-5 font-bold w-screen text-black  '>¿Seguro que quieres realizar la compra? </h1>
+           <h1 className='text-xl text-center  p-5 font-bold w-screen text-white  '>¿Seguro que quieres realizar la compra? </h1>
             <div className="flex justify-center">
                 <button onClick={()=>window.location.href='/Carrito'} className='bg-red-500 text-white w-36 rounded-full p-5 m-1' >Cancelar</button>
                 <button onClick={handleSubmit} className='bg-green-500  text-white w-36 p-5 m-1  rounded-full ' >Aceptar</button>

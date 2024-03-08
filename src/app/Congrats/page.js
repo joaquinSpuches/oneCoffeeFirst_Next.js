8f8ce7e91dab62a7d30477ab9433dce14ca0078c
joaquinSpuches <spuchesjoaquin@gmail.com>
2024-03-08 04:13:58 +0100

    agregado el total en el carrito y un directorio con las compras realizadas por el usario, le falta css

--- src/app/Congrats/page.js
+++ src/app/Congrats/page.js
@@ -5,12 +5,12 @@ import Image from "next/image"
 
 export default function page(params) {
     return(
-        <div className="h-screen flex flex-col text-white  bg-green-600 justify-around items-center">
+        <div className="h-screen flex flex-col text-white  bg-green-600 justify-center items-center">
             <div className="flex flex-col items-center">
             <Title text='Felicitaciones'/>
             <h2 className=" text-xl font-bold  ">¡Haz completado tu compra!</h2>
             </div>
-            <Image src="https://firebasestorage.googleapis.com/v0/b/onecoffeefirst-5da09.appspot.com/o/Touch%20ID%20Symbol.png?alt=media&token=5619aa3a-6a48-4752-87ef-4471dac61c2e" width={84} height={86} alt="Compra Exitosa"/>
+            <Image priority src="https://firebasestorage.googleapis.com/v0/b/onecoffeefirst-5da09.appspot.com/o/Touch%20ID%20Symbol.png?alt=media&token=5619aa3a-6a48-4752-87ef-4471dac61c2e" width={84} height={86} alt="Compra Exitosa"/>
            <div className="flex flex-col gap-5 items-center">
            <Link className=" transition ease-in  hover:bg-[#ECDCC8] p-3 border-[0.5px] border-white rounded-lg w-[200px] text-center" href="/">Ir al Inicio</Link>
             <Link className=" transition ease-in  hover:bg-[#ECDCC8] p-3 border-[0.5px] border-white rounded-lg w-[200px] text-center"  href="/Perfil/Compras">Ver mis compras</Link>

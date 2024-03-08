ad17f6e49659895278a5be2afc1641f7308dac15
joaquinSpuches <spuchesjoaquin@gmail.com>
2024-03-08 04:18:33 +0100

    css en el congrats

--- src/app/Congrats/page.js
+++ src/app/Congrats/page.js
@@ -10,7 +10,7 @@ export default function page(params) {
             <Title text='Felicitaciones'/>
             <h2 className=" text-xl font-bold  ">¡Haz completado tu compra!</h2>
             </div>
-            <Image priority src="https://firebasestorage.googleapis.com/v0/b/onecoffeefirst-5da09.appspot.com/o/Touch%20ID%20Symbol.png?alt=media&token=5619aa3a-6a48-4752-87ef-4471dac61c2e" width={84} height={86} alt="Compra Exitosa"/>
+            <Image className="py-24" priority src="https://firebasestorage.googleapis.com/v0/b/onecoffeefirst-5da09.appspot.com/o/Touch%20ID%20Symbol.png?alt=media&token=5619aa3a-6a48-4752-87ef-4471dac61c2e" width={84} height={86} alt="Compra Exitosa"/>
            <div className="flex flex-col gap-5 items-center">
            <Link className=" transition ease-in  hover:bg-[#ECDCC8] p-3 border-[0.5px] border-white rounded-lg w-[200px] text-center" href="/">Ir al Inicio</Link>
             <Link className=" transition ease-in  hover:bg-[#ECDCC8] p-3 border-[0.5px] border-white rounded-lg w-[200px] text-center"  href="/Perfil/Compras">Ver mis compras</Link>

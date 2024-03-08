a65f9121f40af7349885280d29fed8c20dae3cfa
joaquinSpuches <spuchesjoaquin@gmail.com>
2024-03-08 04:10:09 +0100

    agregado el total en el carrito y un directorio con las compras realizadas por el usario, le falta css

--- src/app/Register/@login/default.js
+++ src/app/Register/@login/default.js
@@ -1,6 +1,9 @@
 'use client'
 import { useState } from 'react';
 import FormularioLogin from "@/Components/auth/FormularioLogin";
+import Title from "@/Components/Title";
+import Link from 'next/link';
+
 
 export default function Page() {
     const [showDenied, setShowDenied] = useState(false);
@@ -11,15 +14,15 @@ export default function Page() {
     }, 2000);
 
     return (
-        <div>
-            {showDenied ? (
-                <div>
-                    <h1>Ya esta registrado!</h1>
-                    <a href="/Perfil">Ir a mi Perfil</a>
-                </div>
-            ) : (
-                <div>Cargando...</div>
-            )}
-        </div>
+       <div className="h-screen flex flex-col text-white  bg-green-600 justify-around items-center">
+            <div className="flex flex-col items-center">
+            <Title text='Felicitaciones'/>
+            <h2 className=" text-xl font-bold  ">¡Ya estas registrado!</h2>
+            </div>
+           <div className="flex flex-col gap-5 items-center">
+           <Link className=" transition ease-in  hover:bg-[#ECDCC8] p-3 border-[0.5px] border-white rounded-lg w-[200px] text-center" href="/">Ir al Inicio</Link>
+           
+           </div>
+        </div> 
     );
 }
